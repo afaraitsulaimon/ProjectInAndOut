cce75ba01f0d6099843d7a0a1b0c1913dfa631cb
afaraitsulaimon <afaraitsulaimon@gmail.com>
2022-12-14 10:47:11 +0100

    Add project files.

---
+++ ProjectInAndOut/Controllers/ItemController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectInAndOut.Data;
+using ProjectInAndOut.Models;
+
+namespace ProjectInAndOut.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objList = _db.Items;
+            return View(objList);
+        }
+
+        //Get create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post Create, which means posting the form data
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item obj)
+        {
+            _db.Items.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ProjectInAndOut/Models/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace ProjectInAndOut.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public string Borrower { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public string Lender { get; set; }
+
+        [DisplayName("Item name")]
+        public string ItemName { get; set; }
+    }
+}
